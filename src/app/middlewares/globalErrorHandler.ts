c66c6b5364dbbc1478cb97e4e7ba5e6c4a678fd9
Rafi <itzmeh320@gmail.com>
2025-07-23 22:01:18 +0600

    Cloudinary and Multer setup

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,9 +7,18 @@ import { handleDuplicateError } from "../helpers/handleDuplicateError";
 import { handleCastError } from "../helpers/handleCastError";
 import { handleZodError } from "../helpers/handlerZodError";
 import { handleValidationError } from "../helpers/handlerValidationError";
+import { deleteImageFromCloudinary } from "../config/cloudinary.config";
 
 
-export const globalErrorHandler = (err: any, req: Request, res: Response,next: NextFunction)=>{
+export const globalErrorHandler =async (err: any, req: Request, res: Response,next: NextFunction)=>{
+    if(req.file){
+        await deleteImageFromCloudinary(req.file.path)
+    }
+    if(req.files && Array.isArray(req.files) && req.files.length > 0){
+      const imageUrls = (req.files as Express.Multer.File[]).map(file => file.path)
+      await Promise.all(imageUrls.map(url => deleteImageFromCloudinary(url)))
+    }
+
     let statusCode = 500;
     let message = "Something Went Wrong!!";
 
